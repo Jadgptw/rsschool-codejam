b22571f10a6fa715b8296322b3166f36b1457f4a
Jadgptw <Gordin.Alexander90@gmail.com>
2018-10-28 01:13:41 +0300

    refactor: Change 8 spaces to 4 spaces in sumOfOther.js

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,11 +1,10 @@
 module.exports = function sumOfOther(array) {
-        const result = [];
-        array.forEach(i => {
-                result.push(array
-                    .filter(y => y !== i)
-                    .reduce((sum, x) => sum + x, 0)
-                )
-            }
-        );
-        return result;
+    const result = [];
+    array.forEach(i => {
+        result.push(array
+            .filter(y => y !== i)
+            .reduce((sum, x) => sum + x, 0)
+        )
+    });
+    return result;
 };
