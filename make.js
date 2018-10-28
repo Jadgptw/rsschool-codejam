2b84a6fe788bf94a0863e83b208397bd1326a5f8
Jadgptw <Gordin.Alexander90@gmail.com>
2018-10-28 04:01:37 +0300

    refactor: Small code refactoring of make.js

--- make.js
+++ make.js
@@ -2,9 +2,7 @@ module.exports = function make(...args) {
     const numbers = [];
     const func = ((...args) => {
         if (args.length === 1 && args[0] instanceof Function){
-            const result = numbers.reduce((sum, x) => args[0](sum, x), 0);
-            numbers.length = 0;
-            return result;
+            return numbers.reduce((sum, x) => args[0](sum, x), 0);
         }
         args.forEach(x => numbers.push(x));
         return func;
